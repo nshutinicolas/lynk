3efc7876722cdf2561afcfd60cbcbc0172b03da3
nshutinicolas <nshuti.nicolas@yahoo.com>
2025-04-09 17:35:35 +0200

    Initial Commit

---
+++ Lynk/LynkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LynkApp.swift
+//  Lynk
+//
+//  Created by Musoni nshuti Nicolas on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LynkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
