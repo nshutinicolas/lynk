47088407013221a8a4b5f23167b3602baee1627c
nshutinicolas <nshuti.nicolas@yahoo.com>
2025-09-10 09:43:40 +0200

    Updated Github links to match the new repository

--- Lynk/Constants/Constants.swift
+++ Lynk/Constants/Constants.swift
@@ -10,12 +10,12 @@ import Foundation
 enum AppConstants {
 	static let coreDataStorage = "Bookmarks.sqlite"
 	static let appGroup = "group.com.ernest.lynkapp"
-	static let termsAndConditions = "https://github.com/nshutinicolas/lynk_saver/blob/main/terms_and_condition.md"
-	static let privacyPolicy = "https://github.com/nshutinicolas/lynk_saver/blob/main/privacy_policy.md"
+	static let termsAndConditions = "https://github.com/nshutinicolas/lynk/blob/main/terms_and_condition.md"
+	static let privacyPolicy = "https://github.com/nshutinicolas/lynk/blob/main/privacy_policy.md"
 	static let supportEmail = "nshuti.nicolas@yahoo.com"
-	static let githubUrl = "https://github.com/nshutinicolas/lynk_saver"
+	static let githubUrl = "https://github.com/nshutinicolas/lynk"
 	static let appStoreUrl = "https://apps.apple.com/app/id6746718631"
 	static let appStoreReviewUrl = "https://apps.apple.com/app/id6746718631?action=write-review"
 	static let icloudContainer = "iCloud.rw.lynk.app.ernest.ios"
-	static let howtoDoc = "https://github.com/nshutinicolas/lynk_saver/blob/main/how_to.md"
+	static let howtoDoc = "https://github.com/nshutinicolas/lynk/blob/main/how_to.md"
 }
